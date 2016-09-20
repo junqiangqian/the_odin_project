c685360e026f4f6c8bc6e84137175e26106ecb7a
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-09-20 19:18:46 +0100

    Basic 16x16 grid setup

--- webdevelopment101/etch-a-sketch/js/script.js
+++ webdevelopment101/etch-a-sketch/js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-    
+    var rows = 16;
+    var cols = 16;
+    var gridWidth = $('#grid').width();
+    var gridHeight = $('#grid').height();
+    makeGrid(rows, cols);
+    var squares = $('.square');
+    var sideLength = (gridWidth / cols) -
+                                 2 * parseInt(squares.css("border-left-width"));
+    $('.square').width(sideLength);
+    $('.square').height(sideLength);
+
 });
+
+function makeGrid(rows, cols) {
+    for (var i = 0; i < rows; i++) {
+        var row = '<div class="row">';
+        for (var j = 0; j < cols; j++) {
+            row += '<div class="square"></div>';
+        }
+        $('#grid').append(row);
+    }
+}
