ad20962be5a0d4acf612671568205f0a3d94560e
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-09-22 18:12:53 +0100

    Added current mode display and some styling

--- webdevelopment101/etch-a-sketch/js/script.js
+++ webdevelopment101/etch-a-sketch/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    var modeNames = {'handleDefaultMode' : 'Default Mode',
+                     'handleRandomColourMode' : 'Random Colour Mode',
+                     'handleOpacityMode' : 'Opacity Mode'};
     $('#cleargrid').click(handleClearGrid);
     $('#default').click(function() {
         handleChangeMode(handleDefaultMode);
@@ -10,70 +13,73 @@ $(document).ready(function() {
         handleChangeMode(handleOpacityMode);
     });
     makeGrid(32, 32);
-});
 
-function handleChangeMode(mode) {
-    $('.square').off('mouseenter');
-    $('.square').on('mouseenter', mode);
-}
+    /* Function definitions START */
+    function handleChangeMode(mode) {
+        $('#mode').text(modeNames[mode.name]);
+        $('.square').off('mouseenter');
+        $('.square').on('mouseenter', mode);
+    }
 
-function makeGrid(rows, cols) {
-    for (var i = 0; i < rows; i++) {
-        var row = '<div class="row">';
-        for (var j = 0; j < cols; j++) {
-            row += '<div class="square"></div>';
+    function makeGrid(rows, cols) {
+        for (var i = 0; i < rows; i++) {
+            var row = '<div class="row">';
+            for (var j = 0; j < cols; j++) {
+                row += '<div class="square"></div>';
+            }
+            $('#grid').append(row);
         }
-        $('#grid').append(row);
+        var squares = $('.square');
+        var gridWidth = $('#grid').width();
+        var gridHeight = $('#grid').height();
+        var borderOffset = 0;
+        var sideWidth = (gridWidth / rows) - borderOffset;
+        var sideHeight = (gridHeight / cols) - borderOffset;
+        squares.width(sideWidth);
+        squares.height(sideHeight);
+        handleChangeMode(handleDefaultMode);
     }
-    var squares = $('.square');
-    var gridWidth = $('#grid').width();
-    var gridHeight = $('#grid').height();
-    var borderOffset = 2 * parseInt(squares.css("border-left-width"));
-    var sideWidth = (gridWidth / rows) - borderOffset;
-    var sideHeight = (gridHeight / cols) - borderOffset;
-    squares.width(sideWidth);
-    squares.height(sideHeight);
-    handleChangeMode(handleDefaultMode);
-}
 
-function handleClearGrid() {
-    $('.square').css('background', 'transparent');
-    cols = parseInt(prompt("Enter number of rows"));
-    rows = parseInt(prompt("Enter number of columns"));
-    $('#grid').empty();
-    makeGrid(rows, cols);
-}
-
-function handleDefaultMode() {
-    $(this).css('background', 'black');
-}
-
-function getRandomColour() {
-    var letters = '0123456789ABCDEF';
-    var colour = '#';
-    for (var i = 0; i < 6; i++) {
-        colour += letters[Math.floor(Math.random() * 16)];
+    function handleClearGrid() {
+        $('.square').css('background', 'transparent');
+        cols = parseInt(prompt("Enter number of rows"));
+        rows = parseInt(prompt("Enter number of columns"));
+        $('#grid').empty();
+        makeGrid(rows, cols);
     }
-    return colour;
-}
 
-function handleRandomColourMode() {
-    $(this).css('background', getRandomColour());
-}
+    function handleDefaultMode() {
+        $(this).css('background', 'black');
+    }
 
-function handleOpacityMode() {
-    var cArray = $(this).css("background-color").match(/\d+/g);
-    var nArray = getDarkerShade(cArray);
-    $(this).css('background',
-                'rgb(' + nArray[0] + ',' + nArray[1] + ',' + nArray[2] + ')');
-}
+    function getRandomColour() {
+        var letters = '0123456789ABCDEF';
+        var colour = '#';
+        for (var i = 0; i < 6; i++) {
+            colour += letters[Math.floor(Math.random() * 16)];
+        }
+        return colour;
+    }
 
-function getDarkerShade(cArray) {
-    var nArray = [];
-    cArray.forEach(function(element, index, array) {
-        var nElement = parseInt(element);
-        nElement = Math.max(0, nElement - 25);
-        nArray.push(nElement);
-    });
-    return nArray;
-}
+    function handleRandomColourMode() {
+        $(this).css('background', getRandomColour());
+    }
+
+    function handleOpacityMode() {
+        var cArray = $(this).css("background-color").match(/\d+/g);
+        var nArray = getDarkerShade(cArray);
+        $(this).css(
+            'background',
+            'rgb(' + nArray[0] + ',' + nArray[1] + ',' + nArray[2] + ')');
+    }
+
+    function getDarkerShade(cArray) {
+        var nArray = [];
+        cArray.forEach(function(element, index, array) {
+            var nElement = parseInt(element);
+            nElement = Math.max(0, nElement - 25);
+            nArray.push(nElement);
+        });
+        return nArray;
+    }
+});
