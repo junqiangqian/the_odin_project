86f63c0216c6e0fd913673dd1487fb9a61f440dd
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-12-20 16:51:51 +0000

    simons_says passes

--- webdevelopment101/learn_ruby/03_simon_says/simon_says.rb
+++ webdevelopment101/learn_ruby/03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo(message)
+    message
+end
+
+def shout(message)
+    message.upcase
+end
+
+def repeat(message, times = 2)
+    ([message] * times).join(' ')
+end
+
+def start_of_word(message, n)
+    message[0...n]
+end
+
+def first_word(message)
+    message.split(' ')[0]
+end
+
+def titleize(words)
+    cwords = words.split.map do |word|
+        if %w(the and over).include?(word)
+            word
+        else
+            word.capitalize
+        end
+    end
+    cwords.first.capitalize!
+    cwords.join(' ')
+end
