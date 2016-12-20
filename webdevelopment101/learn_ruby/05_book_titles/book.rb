8639f636480625b00508dc7b01ae038fc505084a
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-12-20 18:57:00 +0000

    book passes

--- webdevelopment101/learn_ruby/05_book_titles/book.rb
+++ webdevelopment101/learn_ruby/05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
-# write your code here
+    attr_accessor :title
+
+    def title=(new_title)
+        exclusions = %w(and the in of a an)
+        @title = new_title.split.map do |word|
+            exclusions.include?(word)? word : word.capitalize
+        end
+        @title[0].capitalize!
+        @title = @title.join(' ')
+    end
 end
