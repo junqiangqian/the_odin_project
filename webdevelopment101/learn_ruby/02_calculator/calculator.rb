249f8901ea67f8865ef82b85fa70c3dcfdbd809b
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-12-20 16:34:18 +0000

    calculator passed with additional test cases

--- webdevelopment101/learn_ruby/02_calculator/calculator.rb
+++ webdevelopment101/learn_ruby/02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(a, b)
+    a + b
+end
+
+def subtract(a, b)
+    a - b
+end
+
+def sum(arr)
+    arr.inject(0, :+)
+end
+
+def multiply(arr)
+    arr.inject(1, :*)
+end
+
+def power(a, b)
+    a ** b
+end
+
+def factorial(i)
+    i < 2? 1 : i * factorial(i - 1)
+end
